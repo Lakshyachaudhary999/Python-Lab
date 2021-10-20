3c7f631d2f71db22ee4ab43be30474f02fe36844
Lakshyachaudhary999 <92726678+Lakshyachaudhary999@users.noreply.github.com>
2021-10-20 21:59:37 +0530

    simple interest and total amount calculation

---
+++ 13.py
@@ -0,0 +1,7 @@
+P=float(input("enter principal"))
+R=float(input("enter rate in percentage"))
+T=float(input("enter time in years"))
+Si=(P*R*T)/100
+print("The simple interset is",Si)
+total_amount=P+Si
+print("the total amount is",total_amount)
